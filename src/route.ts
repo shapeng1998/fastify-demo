102050652b93a8b8c065e94ea7e56f1533ac9508
Peng Sha <jack@shapeng1998.com>
2022-07-01 16:37:23 +0800

    feat: add body validation

--- src/route.ts
+++ src/route.ts
@@ -1,7 +1,23 @@
-import type { FastifyPluginAsync } from 'fastify'
+import type { FastifyPluginAsync, RouteShorthandOptions } from 'fastify'
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    body: {
+      type: 'object',
+      required: ['hello'],
+      properties: {
+        hello: { type: 'number' },
+      },
+    },
+  },
+}
 
 export const routes: FastifyPluginAsync = async (fastify) => {
   fastify.get('/', async () => {
+    return { hello: 'world' }
+  })
+
+  fastify.post('/', opts, async () => {
     return { hello: 'world' }
   })
 }
