e934f98306ba388651fb115de7f79560f7e604c6
Peng Sha <jack@shapeng1998.com>
2022-07-20 13:58:30 +0800

    chore: update deps

--- src/route.ts
+++ src/route.ts
@@ -1,23 +1,7 @@
-import type { FastifyPluginAsync, RouteShorthandOptions } from 'fastify'
-
-const opts: RouteShorthandOptions = {
-  schema: {
-    body: {
-      type: 'object',
-      required: ['hello'],
-      properties: {
-        hello: { type: 'number' },
-      },
-    },
-  },
-}
+import type { FastifyPluginAsync } from 'fastify'
 
 export const routes: FastifyPluginAsync = async (fastify) => {
   fastify.get('/', async () => {
-    return { hello: 'world' }
-  })
-
-  fastify.post('/', opts, async () => {
     return { hello: 'world' }
   })
 }
